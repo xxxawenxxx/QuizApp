47dfd6ed319f986a61fd9910cb9645aab55307a0
theoadhitya <theoadhitya.sw@gmail.com>
2019-03-08 17:58:12 +0700

    adding desc

--- js/abc.js
+++ js/abc.js
@@ -1,9 +1,17 @@
 loadQ();
+/*
+*   Response[i]:
+*   - question_id
+*   - question
+*   - option_A to option_E
+*   - answer
+*/
 function loadQ() {
   var req = new XMLHttpRequest();
   req.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("root").innerHTML = this.responseText;
+      var response = JSON.parse(this.responseText);
+      document.getElementById("root").innerHTML = response[0].question;
     }
   }
   req.open("GET", "data/08-03-2019.json", true);
--- test.php
+++ test.php
@@ -5,6 +5,7 @@ include "controller/question.php";
 $question = (new Question($conn))->getAllQuestion();
 
 $file_name = 'data/'.date('d-m-Y') . '.json';
+
 if (file_put_contents($file_name, $question)) {
     echo $file_name . ' file created';
 } else {
